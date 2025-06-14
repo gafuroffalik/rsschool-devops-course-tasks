719c637ae4a7ef08ae8796b0ff007b789cd31ab1
gafuroffalik <gafuroffalik@gmail.com>
2025-06-15 00:50:05 +0400

    commit task 1

---
+++ terraforms/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+ region = var.region
+}
+
+resource "aws_s3_bucket" "spacelift_test" {
+   bucket = "spacelift-test1"
+   acl = "private"  
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.spacelift_test.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+terraform {
+  backend "s3" {
+    bucket = "spacelift-test1"
+    key    = "global/s3/terraform.tfstate"
+    region = "ca-central-1"
+  }
+}
---
+++ terraforms/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type        = string
+  default     = "dynamic-aws-creds-operator"
+}
+
+variable "region" {
+  type        = string
+  default     = "ca-central-1"
+}
+
+
+variable "ttl" {
+  type        = string
+  default     = "1"
+}
