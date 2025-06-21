1e99c091da27fabfd95d1feaa4f4916214a74d1c
gafuroffalik <gafuroffalik@gmail.com>
2025-06-22 00:18:20 +0400

    Update TF-format

--- terraforms/nat_route.tf
+++ terraforms/nat_route.tf
@@ -8,22 +8,22 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = 2
-  subnet_id = aws_subnet.public[count.index].id
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block  = "0.0.0.0/0"
     instance_id = aws_instance.bastion_nat.id
   }
   tags = { Name = "PrivateRT" }
 }
 
 resource "aws_route_table_association" "private" {
-  count = 2
-  subnet_id = aws_subnet.private[count.index].id
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
--- terraforms/subnets.tf
+++ terraforms/subnets.tf
@@ -1,17 +1,17 @@
 resource "aws_subnet" "public" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
-  tags = { Name = "PublicSubnet-${count.index + 1}" }
+  tags                    = { Name = "PublicSubnet-${count.index + 1}" }
 }
 
 resource "aws_subnet" "private" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = false
-  tags = { Name = "PrivateSubnet-${count.index + 1}" }
+  tags                    = { Name = "PrivateSubnet-${count.index + 1}" }
 }
